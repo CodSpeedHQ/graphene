12f14830f10ea2a1bc857d17cd340c72d6acc8ff
Arthur Pastel <arthur.pastel@gmail.com>
2024-04-15 16:32:50 -0400

    feat: install codspeed

--- setup.py
+++ setup.py
@@ -45,8 +45,9 @@ class PyTest(TestCommand):
 
 
 tests_require = [
-    "pytest>=6,<7",
-    "pytest-benchmark>=3.4,<4",
+    "pytest>=7",
+    "pytest-benchmark>=3.4",
+    "pytest-codspeed",
     "pytest-cov>=3,<4",
     "pytest-mock>=3,<4",
     "pytest-asyncio>=0.16,<2",
